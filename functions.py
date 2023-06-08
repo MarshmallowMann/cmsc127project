d62b68a2804b9443706500f57904ce3c88d34a11
SeanConcepcion29 <siconcepcion@up.edu.ph>
2023-06-08 19:33:29 +0800

    feat: added functions.py file

---
+++ functions.py
@@ -0,0 +1,84 @@
+def printMenu():
+    return
+
+
+# FEATURES
+# Add Feature
+def addExpense():
+    return
+
+
+def addFriend():
+    return
+
+
+def addGroup():
+    return
+
+
+# Delete Feature
+def deleteExpense():
+    return
+
+
+def deleteFriend():
+    return
+
+
+def deleteGroup():
+    return
+
+
+# Search Feature
+def searchExpense():
+    return
+
+
+def searchFriend():
+    return
+
+
+def searchGroup():
+    return
+
+
+# Update Feature
+def updateExpense():
+    return
+
+
+def updateFriend():
+    return
+
+
+def updateGroup():
+    return
+
+
+# REPORTS TO BE GENERATED
+def viewAllExpensesWithinMonth():
+    return
+
+
+def viewAllExpensesWithFriend():
+    return
+
+
+def viewAllExpensesWithGroup():
+    return
+
+
+def viewCurrentBalance():
+    return
+
+
+def viewAllFriendsWithBalance():
+    return
+
+
+def viewAllGroups():
+    return
+
+
+def viewAllGroupsWithBalance():
+    return
