025b766c77a602a8e6d379c9428c4d8879417be7
MarshmallowMann <jcdespi@up.edu.ph>
2023-06-08 18:40:01 +0800

    chore: utilized .env variables for password.

--- README.md
+++ README.md
@@ -15,3 +15,13 @@
 1. Open the command prompt at the root of the project.
 2. Create a new virtual environment using `python -m venv env`
 3. Activate the virtual environment using `env\Scripts\activate.bat`
+
+
+# Setting Environment Variables
+1. Create a new file named `.env` at the root of the project.
+2. Add the following lines to the file:
+```
+PASSWORD={your database password}
+```
+3. Replace `{your database password}` with your database password.
+4. Save the file.
--- main.py
+++ main.py
@@ -1,10 +1,29 @@
 import mariadb as db;
 import sys;
+import os;
+from dotenv import load_dotenv
+
+
 
 
 def main():
+    
     print("Hello World!")
     
+    try:
+        conn = db.connect(
+            user="root",
+            password=os.environ.get('PASSWORD'),
+            host="localhost",
+            database="scott")
+    except db.Error as e:
+        print(f"Error connecting to MariaDB Platform: {e}")
+        sys.exit(1)
+        
+    cur = conn.cursor()
+    print("SUCCESS: Connected to MariaDB Platform")
+    print()
     
 if __name__ == "__main__":
+    load_dotenv()
     main()
