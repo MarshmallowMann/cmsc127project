57faaabbde56d33e651e74f92b11244c43e991f7
Danie-A <draraez@up.edu.ph>
2023-06-09 02:17:35 +0800

    feat: can now add a transaction; still error in adding is_made_by

--- modules/expense.py
+++ modules/expense.py
@@ -6,7 +6,7 @@ def addExpense(cursor: db.Cursor, conn: db.Connection) -> None:
 
 def getUsers(cursor: db.Cursor) -> None:
     try:
-        statement = "SELECT username FROM user"
+        statement = "SELECT username FROM user WHERE user_id != 1"
         cursor.execute(statement)
         print("\n[Users]")
         # iterate over the users
@@ -25,7 +25,7 @@ def getUserCount(cursor: db.Cursor) -> int:
 def get_user_type():
     user_is_lender = None
     while user_is_lender not in [1, 2]:
-        user_is_lender = input("Are you a lender or a borrower?\n[1]Lender\n[2]Borrower\nChoice: ")
+        user_is_lender = input("\nAre you a lender or a borrower?\n[1]Lender\n[2]Borrower\nChoice: ")
         if user_is_lender == '1':
             user_is_lender = 1  # user is a lender
         elif user_is_lender == '2':
@@ -34,13 +34,13 @@ def get_user_type():
             print("\n[ERROR] Invalid Input. Please Choose 1 or 2.\n")
     return user_is_lender
 
-def chooseFriend(user_count) -> str:
+def chooseFriend(user_count) -> int:
     chosen_friend = None
     while chosen_friend is None or not (2 <= chosen_friend <= user_count): # continue to ask user to input correct borrower number 
         try:
             chosen_friend = int(input("\nInput the number for the chosen borrower: "))
             if not (2 <= chosen_friend <= user_count):
-                print("\nInvalid input. Please enter a valid number for the borrower.\n")
+                print("\nInvalid input. Please enter a valid number for the borrower.")
         except ValueError:
             print("\nInvalid input. Please enter a valid integer.\n")
     return chosen_friend
@@ -71,7 +71,7 @@ def get_float_input(prompt: str) -> int:
 def addIsCreatedBy(cursor: db.Cursor, connection: db.Connection, transaction_id, user_id, group_id) -> None:
     try:
         # if group transaction
-        statement = "INSERT INTO is_created_by (transaction_id, user_id, group_id) VALUES (%s, %s, %s)"
+        statement = "INSERT INTO is_created_by (transaction_id, user_id, group_id) VALUES (%d, %d, %d)"
         data = (transaction_id, user_id, group_id)
         cursor.execute(statement, data)
         connection.commit()
@@ -79,30 +79,42 @@ def addIsCreatedBy(cursor: db.Cursor, connection: db.Connection, transaction_id,
     except db.Error as e:
         print(f"Error adding is_created_by group transaction to the database: {e}")
 
-def addIsMadeBy(cursor: db.Cursor, connection: db.Connection, transaction_id, user_id) -> None:
+def addIsMadeBy(cursor: db.Cursor, transaction_id, user_id) -> None:
     try:
         # if user single transaction
-        statement = "INSERT INTO is_made_by(transaction_id, user_id) VALUES (%s, %s)"
+        statement = "INSERT INTO is_made_by(transaction_id, user_id) VALUES (%d, %d)"
         data = (transaction_id, user_id)
         cursor.execute(statement, data)
-        connection.commit()
         print("Successfully added is_made_by single transaction to the database.")
     except db.Error as e:
         print(f"Error adding is_made_by single transaction to the database: {e}")
 
+def getTransactionCount(cursor: db.Cursor) -> int:
+    statement = "SELECT COUNT(DISTINCT transaction_id) FROM transaction"
+    cursor.execute(statement)
+    transaction_count = cursor.fetchone()[0]
+    return transaction_count
+
+def userLendTransaction(cursor: db.Cursor, transaction_amount, transaction_date, transaction_type, isLoan, lender, isPaid, user_id) -> None:
+    try:
+        statement = "INSERT INTO transaction(transaction_amount, transaction_date, transaction_type, isLoan, lender, isPaid, user_id) VALUES (%d, %s,%s,%d,%d,%d,%d)"
+        data = (transaction_amount, transaction_date, transaction_type, isLoan, lender, isPaid, user_id)
+        cursor.execute(statement, data)
+        print("[USER LEND TRANSACTION] Successfully added transaction to the database")
+    except db.Error as e:
+        print(f"[USER LEND TRANSACTION] Error adding transaction to the database: {e}")
 
 def addExpense(cursor: db.Cursor, connection: db.Connection) -> None:
     try:
         transaction_creator = None
-        while transaction_creator not in [1,2]: # 1 = user, 2 = group
-            transaction_creator = input("Choose Transaction Creator:\n[1] User\n[2] Group\nChoice: ")
+        while transaction_creator not in ['1','2']: # 1 = user, 2 = group
+            transaction_creator = input("\nChoose Transaction Creator:\n[1] User\n[2] Group\nChoice: ")
             if transaction_creator=='1': # user transaction
-                # is_made_by 
                 # input transaction amount
                 transaction_amount = None
                 while transaction_amount is None or not (1 <= transaction_amount <= 999999.99):
                     try:
-                        transaction_amount = float(input("Input transaction amount: "))
+                        transaction_amount = float(input("\nInput transaction amount: "))
                         if not (1 <= transaction_amount <= 999999.99):
                             print("\nInvalid input. Please enter a value between 1 and 999999.99.\n")
                     except ValueError:
@@ -113,7 +125,7 @@ def addExpense(cursor: db.Cursor, connection: db.Connection) -> None:
                 
                 transaction_type_choice = None  # Initialize transaction_type with None
 
-                while transaction_type_choice not in [1, 2]:
+                while transaction_type_choice not in ['1', '2']:
                     transaction_type_choice = input("Choose transaction type:\n[1]Loan\n[2]Settlement\nChoice: ")
 
                     # LOAN
@@ -124,16 +136,16 @@ def addExpense(cursor: db.Cursor, connection: db.Connection) -> None:
                         
                         # ask user if borrower or lender
                         userIsLender = get_user_type()
-                                
-                                
+                                    
                         if userIsLender==1: # user is a lender
                             lender = 1
                             # ask user to select a borrower
                             print("Choose a borrower:")
                             getUsers(cursor)  # get the user count using the getUsers() function
-                            chosen_friend = chooseFriend(getUserCount(cursor))  # get the chosen friend using the chooseFriend() function
-                                    
-                                    
+                            user_id = chooseFriend(getUserCount(cursor))  # get the chosen friend using the chooseFriend() function
+                            userLendTransaction(cursor, transaction_amount, transaction_date, transaction_type, isLoan, lender, isPaid, user_id)
+                            addIsMadeBy(cursor, {getTransactionCount}, user_id) # is_made_by to create connection between transaction and user 
+
                         else: # user is a borrower
                             user_id = 1 # user (with user_id 1) is the borrower
                             isLoan = 1
@@ -151,10 +163,8 @@ def addExpense(cursor: db.Cursor, connection: db.Connection) -> None:
 
         # create a sample loan transaction with a lender and a borrower(user is the borrower)
         # ask if user is the borrower or the lender
-
         
         
-
         # print("transaction_amount: \n", transaction_amount)
         # print("transaction_date: \n", transaction_date)
         # print("transaction_type: \n", transaction_type)
@@ -176,15 +186,15 @@ def addExpense(cursor: db.Cursor, connection: db.Connection) -> None:
 #         INSERT INTO transaction(transaction_amount, transaction_date, transaction_type, isLoan, lender, isPaid, user_id) 
 # VALUES(100, '2020-11-11', 'loan', 1, 1, 0, 2);
 
-        statement = "INSERT INTO employees (first_name, last_name) VALUES (%s, %s)"
-        data = (first_name, last_name)
-        cursor.execute(statement, data)
+        # statement = "INSERT INTO employees (first_name, last_name) VALUES (%s, %s)"
+        # data = (first_name, last_name)
+        # cursor.execute(statement, data)
         
         
         connection.commit()
-        print("Successfully added entry to the database")
+        print("[END] Successfully added transaction to the database")
     except db.Error as e:
-        print(f"Error adding entry to the database: {e}")
+        print(f"Error adding transaction to the database: {e}")
     return None
 
 def deleteExpense(cursor: db.Cursor) -> None:
